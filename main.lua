13c19f3a644d813feb96337ae1a8763755eb94e1
Stan O <spammyboy@protonmail.com>
2021-11-23 22:16:15 +0000

    rearrangement for readability

--- main.lua
+++ main.lua
@@ -1,26 +1,12 @@
-function love.load()
-    image = love.graphics.newImage("image.jpg")
-    elapsed = 0
-end
-
-function love.draw()
-    love.graphics.draw(image)
-    crtShader:send("elapsed", elapsed)
-end
-
-function love.update(dt)
-    elapsed = elapsed + dt * 5
-end
-
 crtShader = love.graphics.newShader([[
     extern float elapsed;
-    
+
     vec2 radialDistortion(vec2 coord, float dist) {
         vec2 cc = coord - 0.5;
         dist = dot(cc, cc) * dist + cos(elapsed * .3) * .01;
         return (coord + cc * (1.0 + dist) * dist);
     }
-    
+
     vec4 effect(vec4 color, Image tex, vec2 tc, vec2 pc) {
         vec2 tcr = radialDistortion(tc, .24)  + vec2(.001, 0);
         vec2 tcg = radialDistortion(tc, .20);
@@ -31,9 +17,20 @@ crtShader = love.graphics.newShader([[
         return res * Texel(tex, tcg).a;
     }
 ]])
-    
+
+image = love.graphics.newImage("image.jpg")
+elapsed = 0
+
+function love.draw()
+    love.graphics.draw(image)
+    crtShader:send("elapsed", elapsed)
+    love.graphics.setShader(crtShader)
+end
+
+function love.update(dt)
+    elapsed = elapsed + dt * 5
+end
+
 function love.keypressed(key)
     if key == "escape" then love.event.quit() end
 end
-
-love.graphics.setShader(crtShader)
